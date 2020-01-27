cf4969e79cb0eef037426f6874674b1fb2c12ce3
g1.kwon <spring_shot@naver.com>
2020-01-27 16:57:23 +0900

    add get restaurant detail api

--- rrs-api/src/main/java/kr/co/plateland/rrs/domain/Restaurant.java
+++ rrs-api/src/main/java/kr/co/plateland/rrs/domain/Restaurant.java
@@ -2,19 +2,19 @@ package kr.co.plateland.rrs.domain;
 
 public class Restaurant {
 
-    private final long id;
+    private final Long id;
     private final String name;
     private final String address;
 
     //생성자
-    public Restaurant(long id, String name, String address) {
+    public Restaurant(Long id, String name, String address) {
         //멤버변수
         this.id = id;
         this.name = name;
         this.address = address;
     }
 
-    public long getId() {
+    public Long getId() {
         return id;
     }
 
--- rrs-api/src/main/java/kr/co/plateland/rrs/interfaces/RestaurantController.java
+++ rrs-api/src/main/java/kr/co/plateland/rrs/interfaces/RestaurantController.java
@@ -2,6 +2,7 @@ package kr.co.plateland.rrs.interfaces;
 
 import kr.co.plateland.rrs.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -19,6 +20,29 @@ public class RestaurantController {
         restaurants.add(restaurant);
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        List<Restaurant> restaurants = new ArrayList<>();
+
+        restaurants.add(new Restaurant(1004L, "Bob Jip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+
+        Restaurant restaurant = restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+
+/*      Restaurant restaurant = null;
+
+        if(id == 1004L) {
+            restaurant = new Restaurant(1004L, "Bob Jip", "Seoul");
+        }
+        if(id == 2020L) {
+            restaurant = new Restaurant(2020L, "Cyber Food", "Seoul");
+        }*/
+        return restaurant;
     }
 
 }
