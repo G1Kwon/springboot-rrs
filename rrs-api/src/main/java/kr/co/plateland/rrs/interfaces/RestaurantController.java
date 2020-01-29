c253f03f245bb84131e927f60c3bf927c547c716
G1Kwon <spring_shot@naver.com>
2020-01-29 12:24:32 +0900

    refactoring restaurant list, detail api

--- rrs-api/src/main/java/kr/co/plateland/rrs/interfaces/RestaurantController.java
+++ rrs-api/src/main/java/kr/co/plateland/rrs/interfaces/RestaurantController.java
@@ -1,38 +1,40 @@
 package kr.co.plateland.rrs.interfaces;
 
 import kr.co.plateland.rrs.domain.Restaurant;
+import kr.co.plateland.rrs.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestaurantController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
+
+/*      중복코드를 repository 안에 밀어넣었다.
         List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob Jip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+*/
 
-        Restaurant restaurant = new Restaurant(1004L, "Bob Jip", "Seoul");
-
-        restaurants.add(restaurant);
+        List<Restaurant> restaurants = repository.findAll();
 
         return restaurants;
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-
-        restaurants.add(new Restaurant(1004L, "Bob Jip", "Seoul"));
-        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
-
+        Restaurant restaurant = repository.findById(id);
+/*      중복코드를 Repository에 밀어넣었다.
         Restaurant restaurant = restaurants.stream()
                 .filter(r -> r.getId().equals(id))
                 .findFirst()
-                .orElse(null);
+                .orElse(null);*/
 
 /*      Restaurant restaurant = null;
 
