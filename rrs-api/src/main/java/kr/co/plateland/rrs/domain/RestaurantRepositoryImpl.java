f084cea41e107d207e18cb148e179fc28c1ed986
g1.kwon <spring_shot@naver.com>
2020-02-02 22:16:33 +0900

    구현체와 인터페이스로 분리 - 인터페이스 생성

---
+++ rrs-api/src/main/java/kr/co/plateland/rrs/domain/RestaurantRepositoryImpl.java
@@ -0,0 +1,32 @@
+package kr.co.plateland.rrs.domain;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+    //Refactor -> extract -> Interface
+
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantRepositoryImpl() {
+        restaurants.add(new Restaurant(1004L, "Bob Jip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seoul"));
+    }
+
+    @Override
+    public List<Restaurant> findAll() {
+        return restaurants;
+    }
+
+    @Override
+    public Restaurant findById(Long id) {
+        return restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
